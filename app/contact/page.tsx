8fea7cfcf26d251d4febe040309adb04841a83e5
Pieter Deane <deanepieter@gmail.com>
2024-07-09 06:37:20 +0200

    adding jest config from typescript

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,18 @@
-const Contact = () => {
+/* eslint-disable react/no-unescaped-entities */
+// TODO
+// Provide context for the form
+// Provide form validation
+// Provide form submission
+
+// fetch data from resend - API
+// form validation
+
+export default async function Contact() {
     return (
       <section id="contact" className="py-16 bg-gray-100">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl md:text-4xl font-bold">Contact Me</h2>
+          <h3 className="text-xl md:2xl ">Have a question or want to work together? Fill out the form below and I'll get back to you as soon as possible.</h3>
           <form className="mt-8 space-y-4 max-w-lg mx-auto">
             <div>
               <input
@@ -38,5 +48,5 @@ const Contact = () => {
     );
   }
   
-  export default Contact;
+ 
   
