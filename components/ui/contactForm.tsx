fa5ee81b162052439029933a367cffbce76b578c
Pieter Deane <deanepieter@gmail.com>
2024-07-15 19:47:15 +0200

    added some changes

--- components/ui/contactForm.tsx
+++ components/ui/contactForm.tsx
@@ -29,8 +29,8 @@ const ContactForm: React.FC = () => {
 
     try {
       await emailjs.send(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        process.env.EMAILJS_SERVICE_ID!,
+        process.env.EMAILJS_TEMPLATE_ID!,
         {
           user_name: data.name,
           user_email: data.email,
